d0711a1293daac3382ef1deb3d62deed5fea16f6
Adam <a.6942@yahoo.com>
2021-10-31 21:54:55 -0400

    edited test files

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -3,21 +3,21 @@ const Intern = require('../lib/Intern');
 
 //creating intern object  
 test('creates an Intern object', () => {
-    const intern = new Intern('Nicole', 90, 'nicole.elisaw@gmail', 'SFSU');
+    const intern = new Intern('Adam', 90, 'a.6942@yahoo', 'NYU');
     
     expect(intern.school) .toEqual(expect.any(String));
 });
 
 //gets school from getSchool()
 test('gets employee school', () => {
-    const intern = new Intern('Nicole', 90, 'nicole.elisaw@gmail', 'SFSU');
+    const intern = new Intern('Adam', 90, 'a.6942@yahoo', 'NYU');
     
     expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school.toString()));
 });
 
 //gets role from getRole()
 test('gets role of employee', () => {
-    const intern = new Intern('Nicole', 90, 'nicole.elisaw@gmail.com', 'SFSU');
+    const intern = new Intern('Adam', 90, 'a.6942@yahoo.com', 'NYU');
 
     expect(intern.getRole()).toEqual("Intern");
 }); 
--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -3,14 +3,14 @@ const Manager = require('../lib/Manager');
 
 //creating manager object  
 test('creates an Manager object', () => {
-    const manager = new Manager('Nicole', 90, 'nicole.elisaw@gmail', 4);
+    const manager = new Manager('Adam', 90, 'a.6942@yahoo', 4);
     
     expect(manager.officeNumber).toEqual(expect.any(Number));
 });
 
 //gets role from getRole()
 test('gets role of employee', () => {
-    const manager = new Manager('Nicole', 90, 'nicole.elisaw@gmail.com');
+    const manager = new Manager('Adam', 90, 'a.6942@yahoo.com');
 
     expect(manager.getRole()).toEqual("Manager");
 }); 

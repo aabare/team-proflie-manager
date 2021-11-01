b4ab326d0bca6cddf748393518abb6983eb090a8
Adam <a.6942@yahoo.com>
2021-10-31 22:46:28 -0400

    added employee functions

--- index.js
+++ index.js
@@ -80,3 +80,75 @@ const addManager = () => {
     })
 };
 
+const addEmployee = () => {
+    console.log(`
+    ---------------------------
+    Adding employee to the team
+    ---------------------------
+    `);
+
+    return inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'role',
+            message: "Please choose a role for your employee",
+            choices: ['Engineer, Intern']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is your employee's name?",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log ("Please enter an employee's name.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "Please enter the employee's ID.",
+            validate: nameInput => {
+                if (isNaN(nameInput)) {
+                    console.log("Please enter the employee's ID.")
+                    return false;
+                } else{
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Please enter the employee's email address.",
+            validate: email => {
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                if (valid) {
+                    return true;
+                } else {
+                    console.log ('Please enter an email address.')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "Please enter the employee's github username.",
+            when: (input) => input.role === "Engineer",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log ("Please enter the employee's github username.")
+                }
+            }
+        },
+        {
+            
+        }
+    ])
+}
